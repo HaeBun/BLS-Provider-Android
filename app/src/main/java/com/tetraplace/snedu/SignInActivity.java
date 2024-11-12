cde7636057a84ee8a11c26f8f93ae5b42a21e724
해분 <62525605+HaeBun@users.noreply.github.com>
2024-11-13 00:19:39 +0900

    feat: Sign In 레이아웃 추가
    Sign In 레이아웃 화면 추가 및 Online VScode 테스트

--- app/src/main/java/com/tetraplace/snedu/SignInActivity.java
+++ app/src/main/java/com/tetraplace/snedu/SignInActivity.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.
                 layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.R.id.sign_in), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
